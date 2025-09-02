d75a2e3793d167a30b55d3807c88ed9f6d85fac0
Phạm Khôi Nguyên <100278861+21020083@users.noreply.github.com>
2025-09-02 19:48:43 +0700

    using enum for role

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,12 @@
 class User < ApplicationRecord
   has_secure_password
 
-  ROLES = { admin: 0, user: 1, reader: 2 }
+  enum :role, { admin: 0, user: 1, reader: 2 }
 
   validates :username, presence: true, uniqueness: true
   validates :email, presence: true, uniqueness: true
   validates :password, presence: true, length: { minimum: 8 }, allow_nil: true
-  validates :role, inclusion: { in: ROLES.values }
+  validates :role, inclusion: { in: roles.keys }, allow_nil: true
 
   before_validation :set_name_if_blank, :set_default_role
   after_validation :log_errors
@@ -18,11 +18,11 @@ class User < ApplicationRecord
   private
 
   def set_name_if_blank
-   self.name = username if name.blank?
+    self.name = username if name.blank?
   end
 
   def set_default_role
-    self.role = ROLES[:user] if role.blank?
+    self.role ||= :user
   end
 
   def log_errors
@@ -32,7 +32,7 @@ class User < ApplicationRecord
   end
 
   def check_admin_count
-    if admin? && User.where(role: 0).count == 1
+    if admin? && User.where(role: roles[:admin]).count == 1
       throw :abort
     end
     Rails.logger.info("Checked the admin count")
@@ -46,9 +46,5 @@ class User < ApplicationRecord
 
   def notify_users
     Rails.logger.info("Notifying users about the user destruction")
-  end
-
-  def admin?
-    role == 0
   end
 end
